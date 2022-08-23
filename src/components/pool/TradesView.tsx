34e6ccafa10587da2d1ff212ce02d6e2c4d6833a
Carlo Pascoli <carlo.pascoli@gmail.com>
2022-08-23 13:09:59 +0100

    Add % traded

--- src/components/pool/TradesView.tsx
+++ src/components/pool/TradesView.tsx
@@ -32,14 +32,18 @@ export const TradesView = ( { chainId, poolId, depositToken, investToken } : Poo
 
     const classes = useStyle()
 
+    // arraay of trades made in chronological order
     const swaps = useSwapInfoArray(chainId, poolId)
+
+
+    // chart labels & data
     const label1 = `${depositToken.symbol} % Traded`
     const label2 = `${investToken.symbol} % Traded`
 
-    
-    // chart of cumulative % of tokens traded
+    // cumulative % of tokens traded
     let depositTonensPerc = 0
     let investTokensPerc = 0
+
     const chartData = swaps?.map( (data: any) => {
         const date = data.timestamp * 1000
         const price = parseFloat(fromDecimals(data.feedPrice, 8, 2))
@@ -57,10 +61,15 @@ export const TradesView = ( { chainId, poolId, depositToken, investToken } : Poo
 
         let record : any = {}
         record['time'] = date
-        depositTonensPerc += (data.side === 'BUY') ? -round(100 * bought * price / depositTokenBalance) :  // Bought invest tokens and sold deposit tokens
-                                                      round(100 * sold * price / depositTokenBalance)      // Sold invest tokens and bought deposit tokens
-        investTokensPerc += (data.side === 'BUY') ?   round(100 * bought / investTokenBalance) :           // Bought invest tokens and sold deposit tokens 
-                                                     -round(100 * sold / investTokenBalance)               // Sold invest tokens and bought deposit tokens
+
+        // const stableAssetPercValue = 100 * depositTokenBalance / (investTokenBalance * price + depositTokenBalance)
+        // const riskAssetPercValue = 100 * investTokenBalance / (investTokenBalance * price + depositTokenBalance)
+
+
+        depositTonensPerc += (data.side === 'BUY') ? -round(100 * bought * price / depositTokenBalance) :  //  perc of deposit tokens sold (to buy invest tokens)
+                                                      round(100 * sold * price / depositTokenBalance)      //  perc of deposit tokens bought (by selling invest tokens)
+        investTokensPerc += (data.side === 'BUY') ?   round(100 * bought / investTokenBalance) :           //  perc of invest tokens bought (by selling deposit tokens)
+                                                     -round(100 * sold / investTokenBalance)               //  perc of invest tokens sold (by buying deposit tokens)
         
         record[label1] = round(depositTonensPerc)
         record[label2] = round(investTokensPerc)
@@ -68,30 +77,31 @@ export const TradesView = ( { chainId, poolId, depositToken, investToken } : Poo
     })
 
 
+    // Trades table headers
     const columns: GridColDef[] = [
         { field: 'date', headerName: 'Date', type: 'date', width: 130, sortable: true },
         { field: 'side', headerName: 'Side', width: 90, sortable: false },
         {
-            field: 'asset1',
+            field: 'riskAssetTradedAmount',
             headerName: `${investToken.symbol} Traded`,
             description: 'The amount of the risk asset bought or sold',
-            type: 'number',
+            type: 'string',
             sortable: false,
             width: 150,
-            valueFormatter: (params) => {
-                return params.value;
-            },
+            // valueFormatter: (params) => {
+            //     return params.value;
+            // },
         },
         {
-            field: 'asset2',
+            field: 'stableAssetTradedAmount',
             headerName: `${depositToken.symbol} Traded`,
             description: 'The amount of the stable asset sold or bought',
-            type: 'number',
+            type: 'string',
             sortable: false,
             width: 150,
-            valueFormatter: (params) => {
-                return params.value;
-            },
+            // valueFormatter: (params) => {
+            //     return params.value;
+            // },
           },
           {
             field: 'feedPrice',
@@ -100,24 +110,35 @@ export const TradesView = ( { chainId, poolId, depositToken, investToken } : Poo
             width: 120,
             sortable: false,
         },
-      ];
+    ];
 
-     const rows = swaps?.map( (data: any, index: number) => {
+    // Trades table rows
+    const rows = swaps?.slice().reverse()?.map( (data: any, index: number) => {
+
         const date = new Date(data.timestamp * 1000)
         const feedPrice = parseFloat(fromDecimals(data.feedPrice, 8, 2))
 
-        const factor = data.side === 'BUY' ? 1.0 : -1.0
+        const tradeSideFactor = data.side === 'BUY' ? 1.0 : -1.0
         const amount1 = data.side === 'BUY' ? data.bought : data.sold
         const amount2 = data.side === 'BUY' ? data.sold : data.bought
-        const asset1 = factor * parseFloat(fromDecimals(amount1, investToken.decimals, 8))
-        const asset2 = factor * parseFloat(fromDecimals(amount2, depositToken.decimals, 2))
+        const riskAssetAmountTraded = parseFloat(fromDecimals(amount1, investToken.decimals, 8))
+        const stableAssetAmountTraded =  parseFloat(fromDecimals(amount2, depositToken.decimals, 2))
+
+        // perc risk asset traded
+        const riskAssetBalance = parseFloat(fromDecimals(data.investTokenBalance, investToken.decimals, 8))
+        const riskAssetTradedPerc = round(riskAssetAmountTraded / ( riskAssetBalance + riskAssetAmountTraded))
+
+        // perc stable asset traded
+        const stableAssetBalance = parseFloat(fromDecimals(data.depositTokenBalance, depositToken.decimals, 2))
+        const stableAssetTradedPerc =  round(stableAssetAmountTraded / ( stableAssetBalance + stableAssetAmountTraded))
+
         return {
             id: index,
             date: date,
             side: data.side,
             feedPrice: feedPrice,
-            asset1: asset1,
-            asset2: asset2,
+            riskAssetTradedAmount: `${tradeSideFactor * riskAssetAmountTraded} (${riskAssetTradedPerc}%)`,
+            stableAssetTradedAmount: `${-tradeSideFactor * stableAssetAmountTraded} (${stableAssetTradedPerc}%)`,
         }
     })
   
--- src/components/shared/PoolSummary.tsx
+++ src/components/shared/PoolSummary.tsx
@@ -93,7 +93,6 @@ export const PoolSummary = ({ chainId, poolId, tokens, depositToken, account } :
                     </Box>
                 </Button>
             </Link>
-      
     )
 }
 
